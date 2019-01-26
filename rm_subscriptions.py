726ce1f7cc26df59d8dbfdcb2e68d7e4837a7dc9
Felix Wong <waf@fawong.com>
2019-01-26 10:16:55 +0000

    wow terrible sadness

--- rm_subscriptions.py
+++ rm_subscriptions.py
@@ -3,38 +3,46 @@
 # This code sample shows how to get a channel subscription.
 # python get_subscription.py --channel-id=UC_x5XG1OV2P6uZZ5FSM9Ttw
 
-import argparse
 import os
 import re
 import json
+import httplib2
 
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from oauth2client import tools
+from oauth2client.file import Storage
+from oauth2client.client import AccessTokenRefreshError
+from oauth2client.client import OAuth2WebServerFlow
 
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 #   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 CLIENT_SECRETS_FILE = "client_secret.json"
-CREDS_FILE = "creds"
+CREDS_FILE = "creds.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user"s account.
-SCOPES = ["https://www.googleapis.com/auth/youtube"]
+SCOPE = "https://www.googleapis.com/auth/youtube"
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
 
-def get_authenticated_service():
-    if os.path.isfile(CREDS_FILE):
-        creds = json.loads(open(CREDS_FILE))
-        credentials = creds.Credentials(creds["token"])
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-        credentials = flow.run_console()
+secrets = json.loads(open(CLIENT_SECRETS_FILE).read())
+client_id = secrets["client_id"]
+client_secret = secrets["client_secret"]
 
-    return build(API_SERVICE_NAME, API_VERSION, credentials = credentials)
+flow = OAuth2WebServerFlow(client_id, client_secret, SCOPE)
 
 if __name__ == "__main__":
-    youtube = get_authenticated_service()
+    storage = Storage(CREDS_FILE)
 
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        credentials = tools.run_flow(flow, storage, tools.argparser.parse_args())
+
+    http = httplib2.Http()
+    http = credentials.authorize(http)
+
+    youtube = build(API_SERVICE_NAME, API_VERSION, http=http)
     subscriptions = youtube.subscriptions()
     request = subscriptions.list(part="snippet", mine=True, order="alphabetical")
     while request is not None:
